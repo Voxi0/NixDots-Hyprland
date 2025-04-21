7454892a48d3b72eb8d11a29260200aba7d07084
Voxi0 <alif200099@gmail.com>
2025-04-21 22:41:23 +0100

    Test #27

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
 
 		# Export Home Manager module
 		homeManagerModules = {
-			specialArgs = { inherit inputs; };
+			extraSpecialArgs = { inherit inputs; };
 			default = self.homeManagerModules.nixdots-hyprland;
 			nixdots-hyprland = import ./default.nix { inherit lib inputs; };
 		};
