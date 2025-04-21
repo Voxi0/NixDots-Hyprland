ef53004f36ac6f0e3498883a884428e98ef63266
Voxi0 <alif200099@gmail.com>
2025-04-21 20:19:08 +0100

    Test #13

--- apps/ags/default.nix
+++ apps/ags/default.nix
@@ -1,6 +1,6 @@
 { lib, config, inputs, pkgs, ... }: {
   # Import Nix modules
-  imports = [ inputs.ags.homeManagerModules.default ];
+  imports = [ inputs.self.ags.homeManagerModules.default ];
 
   # Module options
   options.enableAGS = lib.mkEnableOption "AGS (Widgets)";
