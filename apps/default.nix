cbb0bd2fd3728373af54d3f45f1e52044ff61ab8
Voxi0 <alif200099@gmail.com>
2025-04-23 22:37:33 +0100

    Test #57

--- apps/default.nix
+++ apps/default.nix
@@ -1,6 +1,6 @@
 { inputs, ... }: {
   # Import Nix modules
   imports = [
-    ./wofi ./ags { inherit inputs; } ./mako.nix ./wlogout.nix
+    ./wofi (import ./ags { inherit inputs; }) ./mako.nix ./wlogout.nix
   ];
 }
